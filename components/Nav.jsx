56f2e6ce5cb3f655382356dce36d0fc90167e432
Chris Whong <chris@whong.org>
2016-10-31 13:06:32 -0400

    segregate logo and title

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,7 +26,16 @@ var Nav = React.createClass({
 
     return(
       <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="navbar-header"><button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button><a className="navbar-brand" href="#">{this.props.title}</a></div>
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="/"></a>
+          <div className="navbar-title">{this.props.title}</div>
+        </div>
         <div id="navbar" className="navbar-collapse collapse">
            <ul className="nav navbar-nav navbar-right ">
               <li><Link to="/"> Home</Link></li>
