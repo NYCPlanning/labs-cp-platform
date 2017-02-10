5ce2a9f2f25447574594ce8da09a9db8e93ff30a
Chris Whong <chris@whong.org>
2017-02-10 16:30:53 -0500

    checkbox spritesheet working

--- app/facilities/facilitiesjanelayer/Checkbox.jsx
+++ app/facilities/facilitiesjanelayer/Checkbox.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Checkbox = props => (
-  <input
-    type="checkbox"
-    value={props.value}
-    checked={props.checked}
-    onChange={props.onChange}
-    ref={elem => elem && (elem.indeterminate = props.indeterminate)}
-  />
+  <div>
+    <input
+      type="checkbox"
+      value={props.value}
+      checked={props.checked}
+      onChange={props.onChange}
+      ref={elem => elem && (elem.indeterminate = props.indeterminate)}
+    />
+    <span />
+  </div>
 );
 
 Checkbox.propTypes = {
