303fa9e2960c9dd593a3927ee1919fa20fbc5351
Jonathan Pichot <jonathan@pichot.us>
2017-06-08 16:08:02 -0400

    make aerial sidebar component a function

--- app/janelayers/aerials/SidebarComponent.jsx
+++ app/janelayers/aerials/SidebarComponent.jsx
@@ -7,27 +7,22 @@ const tabTemplateStyle = {
   bottom: 0,
 };
 
-class Aerials extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <Tabs
-          className="sidebar-tabs"
-          tabTemplateStyle={tabTemplateStyle}
-        >
-          <Tab label="About">
-            <div className="sidebar-tab-content">
-              <div className="padded">
-                <h4>2016 Aerial Imagery</h4>
-                <p>This tile layer is published by the NYC DoITT GIS Team</p>
-              </div>
-            </div>
-          </Tab>
-        </Tabs>
-      </div>
-    );
-  }
-}
+const Aerials = () => (
+  <div>
+    <Tabs
+      className="sidebar-tabs"
+      tabTemplateStyle={tabTemplateStyle}
+    >
+      <Tab label="About">
+        <div className="sidebar-tab-content">
+          <div className="padded">
+            <h4>2016 Aerial Imagery</h4>
+            <p>This tile layer is published by the NYC DoITT GIS Team</p>
+          </div>
+        </div>
+      </Tab>
+    </Tabs>
+  </div>
+);
 
 export default Aerials;
