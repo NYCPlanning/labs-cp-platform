ab1baa7817b48fb6e3cbf3544731548e672a3b09
Jonathan Pichot <jonathan@pichot.us>
2017-10-05 15:20:06 -0400

    add councilpeople to filtering

--- app/helpers/labels/js/council.js
+++ app/helpers/labels/js/council.js
@@ -1,1 +1,208 @@
-const labels = [{"value":"1","label":"1"},{"value":"2","label":"2"},{"value":"3","label":"3"},{"value":"4","label":"4"},{"value":"5","label":"5"},{"value":"6","label":"6"},{"value":"7","label":"7"},{"value":"8","label":"8"},{"value":"9","label":"9"},{"value":"10","label":"10"},{"value":"11","label":"11"},{"value":"12","label":"12"},{"value":"13","label":"13"},{"value":"14","label":"14"},{"value":"15","label":"15"},{"value":"16","label":"16"},{"value":"17","label":"17"},{"value":"18","label":"18"},{"value":"19","label":"19"},{"value":"20","label":"20"},{"value":"21","label":"21"},{"value":"22","label":"22"},{"value":"23","label":"23"},{"value":"24","label":"24"},{"value":"25","label":"25"},{"value":"26","label":"26"},{"value":"27","label":"27"},{"value":"28","label":"28"},{"value":"29","label":"29"},{"value":"30","label":"30"},{"value":"31","label":"31"},{"value":"32","label":"32"},{"value":"33","label":"33"},{"value":"34","label":"34"},{"value":"35","label":"35"},{"value":"36","label":"36"},{"value":"37","label":"37"},{"value":"38","label":"38"},{"value":"39","label":"39"},{"value":"40","label":"40"},{"value":"41","label":"41"},{"value":"42","label":"42"},{"value":"43","label":"43"},{"value":"44","label":"44"},{"value":"45","label":"45"},{"value":"46","label":"46"},{"value":"47","label":"47"},{"value":"48","label":"48"},{"value":"49","label":"49"},{"value":"50","label":"50"},{"value":"51","label":"51"}]; export default labels;
+const labels = [
+  {
+    "value": "1",
+    "label": "1 - Margaret S. Chin"
+  },
+  {
+    "value": "2",
+    "label": "2 - Rosie Mendez"
+  },
+  {
+    "value": "3",
+    "label": "3 - Corey Johnson"
+  },
+  {
+    "value": "4",
+    "label": "4 - Daniel R. Garodnick"
+  },
+  {
+    "value": "5",
+    "label": "5 - Ben Kallos"
+  },
+  {
+    "value": "6",
+    "label": "6 - Helen Rosenthal"
+  },
+  {
+    "value": "7",
+    "label": "7 - Mark Levine"
+  },
+  {
+    "value": "8",
+    "label": "8 - Melissa Mark-Viverito"
+  },
+  {
+    "value": "9",
+    "label": "9 - Bill Perkins"
+  },
+  {
+    "value": "10",
+    "label": "10 - Ydanis Rodriguez"
+  },
+  {
+    "value": "11",
+    "label": "11 - Andrew Cohen"
+  },
+  {
+    "value": "12",
+    "label": "12 - Andy King"
+  },
+  {
+    "value": "13",
+    "label": "13 - James Vacca"
+  },
+  {
+    "value": "14",
+    "label": "14 - Fernando Cabrera"
+  },
+  {
+    "value": "15",
+    "label": "15 - Ritchie J. Torres"
+  },
+  {
+    "value": "16",
+    "label": "16 - Vanessa L. Gibson"
+  },
+  {
+    "value": "17",
+    "label": "17 - Rafael Salamanca Jr"
+  },
+  {
+    "value": "18",
+    "label": "18 - Annabel Palma"
+  },
+  {
+    "value": "19",
+    "label": "19 - Paul Vallone"
+  },
+  {
+    "value": "20",
+    "label": "20 - Peter Koo"
+  },
+  {
+    "value": "21",
+    "label": "21 - Julissa Ferreras-Copeland"
+  },
+  {
+    "value": "22",
+    "label": "22 - Costa Constantinides"
+  },
+  {
+    "value": "23",
+    "label": "23 - Barry Grodenchik"
+  },
+  {
+    "value": "24",
+    "label": "24 - Rory I. Lancman"
+  },
+  {
+    "value": "25",
+    "label": "25 - Daniel Dromm"
+  },
+  {
+    "value": "26",
+    "label": "26 - Jimmy Van Bramer"
+  },
+  {
+    "value": "27",
+    "label": "27 - I. Daneek Miller"
+  },
+  {
+    "value": "28",
+    "label": "28 - Vacant"
+  },
+  {
+    "value": "29",
+    "label": "29 - Karen Koslowitz"
+  },
+  {
+    "value": "30",
+    "label": "30 - Elizabeth S. Crowley"
+  },
+  {
+    "value": "31",
+    "label": "31 - Donovan J. Richards"
+  },
+  {
+    "value": "32",
+    "label": "32 - Eric A. Ulrich"
+  },
+  {
+    "value": "33",
+    "label": "33 - Stephen T. Levin"
+  },
+  {
+    "value": "34",
+    "label": "34 - Antonio Reynoso"
+  },
+  {
+    "value": "35",
+    "label": "35 - Laurie A. Cumbo"
+  },
+  {
+    "value": "36",
+    "label": "36 - Robert E. Cornegy Jr."
+  },
+  {
+    "value": "37",
+    "label": "37 - Rafael L. Espinal Jr."
+  },
+  {
+    "value": "38",
+    "label": "38 - Carlos Menchaca"
+  },
+  {
+    "value": "39",
+    "label": "39 - Brad Lander"
+  },
+  {
+    "value": "40",
+    "label": "40 - Mathieu Eugene"
+  },
+  {
+    "value": "41",
+    "label": "41 - Darlene Mealy"
+  },
+  {
+    "value": "42",
+    "label": "42 - Inez Barron"
+  },
+  {
+    "value": "43",
+    "label": "43 - Vincent Gentile"
+  },
+  {
+    "value": "44",
+    "label": "44 - David G. Greenfield"
+  },
+  {
+    "value": "45",
+    "label": "45 - Jumaane D. Williams"
+  },
+  {
+    "value": "46",
+    "label": "46 - Alan N. Maisel"
+  },
+  {
+    "value": "47",
+    "label": "47 - Mark Treyger"
+  },
+  {
+    "value": "48",
+    "label": "48 - Chaim M. Deutsch"
+  },
+  {
+    "value": "49",
+    "label": "49 - Deborah Rose"
+  },
+  {
+    "value": "50",
+    "label": "50 - Steven Matteo"
+  },
+  {
+    "value": "51",
+    "label": "51 - Joseph C. Borelli"
+  }
+];
+
+export default labels;
