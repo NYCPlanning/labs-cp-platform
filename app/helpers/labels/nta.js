755be63a79ca7701c2492bfbeef75d047442a655
Jonathan Pichot <jonathan@pichot.us>
2017-10-26 12:02:35 -0400

    Add NTA names

---
+++ app/helpers/labels/nta.js
@@ -0,0 +1,783 @@
+const   labels = [{
+    value: 'BX98',
+  label: 'BX98 - Rikers Island'
+},
+{
+  value: 'BK90',
+  label: 'BK90 - East Williamsburg'
+},
+{
+  value: 'BK93',
+  label: 'BK93 - Starrett City'
+},
+{
+  value: 'QN72',
+  label: 'QN72 - Steinway'
+},
+{
+  value: 'QN23',
+  label: 'QN23 - College Point'
+},
+{
+  value: 'BX27',
+  label: 'BX27 - Hunts Point'
+},
+{
+  value: 'MN99',
+  label: 'MN99 - Manhattan (Parks, Cemeteries, etc)'
+},
+{
+  value: 'QN53',
+  label: 'QN53 - Woodhaven'
+},
+{
+  value: 'SI05',
+  label: 'SI05 - New Springville-Bloomfield-Travis'
+},
+{
+  value: 'BK25',
+  label: 'BK25 - Homecrest'
+},
+{
+  value: 'BK95',
+  label: 'BK95 - Erasmus'
+},
+{
+  value: 'BK43',
+  label: 'BK43 - Midwood'
+},
+{
+  value: 'SI99',
+  label: 'SI99 - Staten Island (Parks, Cemeteries, etc)'
+},
+{
+  value: 'BX52',
+  label: 'BX52 - Schuylerville-Throgs Neck-Edgewater Park'
+},
+{
+  value: 'QN49',
+  label: 'QN49 - Whitestone'
+},
+{
+  value: 'SI54',
+  label: 'SI54 - Great Kills'
+},
+{
+  value: 'BK17',
+  label: 'BK17 - Sheepshead Bay-Gerritsen Beach-Manhattan Beach'
+},
+{
+  value: 'BX10',
+  label: 'BX10 - Pelham Bay-Country Club-City Island'
+},
+{
+  value: 'BK45',
+  label: 'BK45 - Georgetown-Marine Park-Bergen Beach-Mill Basin'
+},
+{
+  value: 'SI12',
+  label: 'SI12 - Mariner's Harbor-Arlington-Port Ivory-Graniteville'
+},
+{
+  value: 'QN26',
+  label: 'QN26 - North Corona'
+},
+{
+  value: 'SI01',
+  label: 'SI01 - Annadale-Huguenot-Prince's Bay-Eltingville'
+},
+{
+  value: 'SI11',
+  label: 'SI11 - Charleston-Richmond Valley-Tottenville'
+},
+{
+  value: 'QN45',
+  label: 'QN45 - Douglas Manor-Douglaston-Little Neck'
+},
+{
+  value: 'QN31',
+  label: 'QN31 - Hunters Point-Sunnyside-West Maspeth'
+},
+{
+  value: 'SI22',
+  label: 'SI22 - West New Brighton-New Brighton-St. George'
+},
+{
+  value: 'QN10',
+  label: 'QN10 - Breezy Point-Belle Harbor-Rockaway Park-Broad Channel'
+},
+{
+  value: 'QN12',
+  label: 'QN12 - Hammels-Arverne-Edgemere'
+},
+{
+  value: 'MN22',
+  label: 'MN22 - East Village'
+},
+{
+  value: 'QN15',
+  label: 'QN15 - Far Rockaway-Bayswater'
+},
+{
+  value: 'QN57',
+  label: 'QN57 - Lindenwood-Howard Beach'
+},
+{
+  value: 'QN05',
+  label: 'QN05 - Rosedale'
+},
+{
+  value: 'QN98',
+  label: 'QN98 - Airport'
+},
+{
+  value: 'BK09',
+  label: 'BK09 - Brooklyn Heights-Cobble Hill'
+},
+{
+  value: 'BX33',
+  label: 'BX33 - Longwood'
+},
+{
+  value: 'BK72',
+  label: 'BK72 - Williamsburg'
+},
+{
+  value: 'BX40',
+  label: 'BX40 - Fordham South'
+},
+{
+  value: 'MN25',
+  label: 'MN25 - Battery Park City-Lower Manhattan'
+},
+{
+  value: 'BX09',
+  label: 'BX09 - Soundview-Castle Hill-Clason Point-Harding Park'
+},
+{
+  value: 'BX99',
+  label: 'BX99 - Bronx (Parks, Cemeteries, etc) '
+},
+{
+  value: 'MN21',
+  label: 'MN21 - Gramercy'
+},
+{
+  value: 'MN17',
+  label: 'MN17 - Midtown-Midtown South'
+},
+{
+  value: 'QN41',
+  label: 'QN41 - Fresh Meadows-Utopia'
+},
+{
+  value: 'QN18',
+  label: 'QN18 - Rego Park'
+},
+{
+  value: 'BX31',
+  label: 'BX31 - Allerton-Pelham Gardens'
+},
+{
+  value: 'BK28',
+  label: 'BK28 - Bensonhurst West'
+},
+{
+  value: 'BK63',
+  label: 'BK63 - Crown Heights South'
+},
+{
+  value: 'BK27',
+  label: 'BK27 - Bath Beach'
+},
+{
+  value: 'QN33',
+  label: 'QN33 - Cambria Heights'
+},
+{
+  value: 'BK77',
+  label: 'BK77 - Bushwick North'
+},
+{
+  value: 'BX01',
+  label: 'BX01 - Claremont-Bathgate'
+},
+{
+  value: 'BX43',
+  label: 'BX43 - Norwood'
+},
+{
+  value: 'BX13',
+  label: 'BX13 - Co-op City'
+},
+{
+  value: 'MN04',
+  label: 'MN04 - Hamilton Heights'
+},
+{
+  value: 'SI24',
+  label: 'SI24 - Todt Hill-Emerson Hill-Heartland Village-Lighthouse Hill'
+},
+{
+  value: 'BK21',
+  label: 'BK21 - Seagate-Coney Island'
+},
+{
+  value: 'BK82',
+  label: 'BK82 - East New York'
+},
+{
+  value: 'BK50',
+  label: 'BK50 - Canarsie'
+},
+{
+  value: 'MN01',
+  label: 'MN01 - Marble Hill-Inwood'
+},
+{
+  value: 'MN11',
+  label: 'MN11 - Central Harlem South'
+},
+{
+  value: 'BK32',
+  label: 'BK32 - Sunset Park West'
+},
+{
+  value: 'BK99',
+  label: 'BK99 - Brooklyn (Parks, Cemeteries, etc) '
+},
+{
+  value: 'QN99',
+  label: 'QN99 - Queens (Parks, Cemeteries, etc)'
+},
+{
+  value: 'BK69',
+  label: 'BK69 - Clinton Hill'
+},
+{
+  value: 'MN28',
+  label: 'MN28 - Lower East Side'
+},
+{
+  value: 'QN07',
+  label: 'QN07 - Hollis'
+},
+{
+  value: 'BK44',
+  label: 'BK44 - Madison'
+},
+{
+  value: 'QN56',
+  label: 'QN56 - Ozone Park'
+},
+{
+  value: 'BK60',
+  label: 'BK60 - Prospect Lefferts Gardens-Wingate'
+},
+{
+  value: 'BK88',
+  label: 'BK88 - Borough Park'
+},
+{
+  value: 'QN52',
+  label: 'QN52 - East Flushing'
+},
+{
+  value: 'QN48',
+  label: 'QN48 - Auburndale'
+},
+{
+  value: 'QN51',
+  label: 'QN51 - Murray Hill'
+},
+{
+  value: 'QN27',
+  label: 'QN27 - East Elmhurst'
+},
+{
+  value: 'BX35',
+  label: 'BX35 - Morrisania-Melrose'
+},
+{
+  value: 'MN06',
+  label: 'MN06 - Manhattanville'
+},
+{
+  value: 'QN02',
+  label: 'QN02 - Springfield Gardens North'
+},
+{
+  value: 'QN71',
+  label: 'QN71 - Old Astoria'
+},
+{
+  value: 'QN55',
+  label: 'QN55 - South Ozone Park'
+},
+{
+  value: 'QN46',
+  label: 'QN46 - Bayside-Bayside Hills'
+},
+{
+  value: 'QN38',
+  label: 'QN38 - Pomonok-Flushing Heights-Hillcrest'
+},
+{
+  value: 'MN15',
+  label: 'MN15 - Clinton'
+},
+{
+  value: 'BK46',
+  label: 'BK46 - Ocean Parkway South'
+},
+{
+  value: 'BX59',
+  label: 'BX59 - Westchester-Unionport'
+},
+{
+  value: 'MN19',
+  label: 'MN19 - Turtle Bay-East Midtown'
+},
+{
+  value: 'QN08',
+  label: 'QN08 - St. Albans'
+},
+{
+  value: 'QN54',
+  label: 'QN54 - Richmond Hill'
+},
+{
+  value: 'QN25',
+  label: 'QN25 - Corona'
+},
+{
+  value: 'QN47',
+  label: 'QN47 - Ft. Totten-Bay Terrace-Clearview'
+},
+{
+  value: 'BK41',
+  label: 'BK41 - Kensington-Ocean Parkway'
+},
+{
+  value: 'QN37',
+  label: 'QN37 - Kew Gardens Hills'
+},
+{
+  value: 'SI45',
+  label: 'SI45 - New Dorp-Midland Beach'
+},
+{
+  value: 'QN06',
+  label: 'QN06 - Jamaica Estates-Holliswood'
+},
+{
+  value: 'BK40',
+  label: 'BK40 - Windsor Terrace'
+},
+{
+  value: 'BK76',
+  label: 'BK76 - Greenpoint'
+},
+{
+  value: 'QN30',
+  label: 'QN30 - Maspeth'
+},
+{
+  value: 'QN35',
+  label: 'QN35 - Briarwood-Jamaica Hills'
+},
+{
+  value: 'QN61',
+  label: 'QN61 - Jamaica'
+},
+{
+  value: 'QN76',
+  label: 'QN76 - Baisley Park'
+},
+{
+  value: 'QN63',
+  label: 'QN63 - Woodside'
+},
+{
+  value: 'QN01',
+  label: 'QN01 - South Jamaica'
+},
+{
+  value: 'BK34',
+  label: 'BK34 - Sunset Park East'
+},
+{
+  value: 'QN70',
+  label: 'QN70 - Astoria'
+},
+{
+  value: 'QN34',
+  label: 'QN34 - Queens Village'
+},
+{
+  value: 'QN66',
+  label: 'QN66 - Laurelton'
+},
+{
+  value: 'BK19',
+  label: 'BK19 - Brighton Beach'
+},
+{
+  value: 'SI48',
+  label: 'SI48 - Arden Heights'
+},
+{
+  value: 'BK26',
+  label: 'BK26 - Gravesend'
+},
+{
+  value: 'BK29',
+  label: 'BK29 - Bensonhurst East'
+},
+{
+  value: 'SI28',
+  label: 'SI28 - Port Richmond'
+},
+{
+  value: 'MN32',
+  label: 'MN32 - Yorkville'
+},
+{
+  value: 'BK85',
+  label: 'BK85 - East New York (Pennsylvania Ave)'
+},
+{
+  value: 'BK75',
+  label: 'BK75 - Bedford'
+},
+{
+  value: 'SI32',
+  label: 'SI32 - Rossville-Woodrow'
+},
+{
+  value: 'BK58',
+  label: 'BK58 - Flatlands'
+},
+{
+  value: 'BK81',
+  label: 'BK81 - Brownsville'
+},
+{
+  value: 'BK79',
+  label: 'BK79 - Ocean Hill'
+},
+{
+  value: 'BK61',
+  label: 'BK61 - Crown Heights North'
+},
+{
+  value: 'MN20',
+  label: 'MN20 - Murray Hill-Kips Bay'
+},
+{
+  value: 'BK35',
+  label: 'BK35 - Stuyvesant Heights'
+},
+{
+  value: 'BK96',
+  label: 'BK96 - Rugby-Remsen Village'
+},
+{
+  value: 'BX07',
+  label: 'BX07 - Bronxdale'
+},
+{
+  value: 'BX44',
+  label: 'BX44 - Williamsbridge-Olinville'
+},
+{
+  value: 'BX03',
+  label: 'BX03 - Eastchester-Edenwald-Baychester'
+},
+{
+  value: 'BK91',
+  label: 'BK91 - East Flatbush-Farragut'
+},
+{
+  value: 'QN62',
+  label: 'QN62 - Queensboro Hill'
+},
+{
+  value: 'BK30',
+  label: 'BK30 - Dyker Heights'
+},
+{
+  value: 'SI37',
+  label: 'SI37 - Stapleton-Rosebank'
+},
+{
+  value: 'MN23',
+  label: 'MN23 - West Village'
+},
+{
+  value: 'SI07',
+  label: 'SI07 - Westerleigh'
+},
+{
+  value: 'MN13',
+  label: 'MN13 - Hudson Yards-Chelsea-Flatiron-Union Square'
+},
+{
+  value: 'BX41',
+  label: 'BX41 - Mount Hope'
+},
+{
+  value: 'BK78',
+  label: 'BK78 - Bushwick South'
+},
+{
+  value: 'QN20',
+  label: 'QN20 - Ridgewood'
+},
+{
+  value: 'QN43',
+  label: 'QN43 - Bellerose'
+},
+{
+  value: 'BX06',
+  label: 'BX06 - Belmont'
+},
+{
+  value: 'QN44',
+  label: 'QN44 - Glen Oaks-Floral Park-New Hyde Park'
+},
+{
+  value: 'BX34',
+  label: 'BX34 - Melrose South-Mott Haven North'
+},
+{
+  value: 'QN22',
+  label: 'QN22 - Flushing'
+},
+{
+  value: 'MN24',
+  label: 'MN24 - SoHo-TriBeCa-Civic Center-Little Italy'
+},
+{
+  value: 'BK68',
+  label: 'BK68 - Fort Greene'
+},
+{
+  value: 'MN27',
+  label: 'MN27 - Chinatown'
+},
+{
+  value: 'MN50',
+  label: 'MN50 - Stuyvesant Town-Cooper Village'
+},
+{
+  value: 'MN40',
+  label: 'MN40 - Upper East Side-Carnegie Hill'
+},
+{
+  value: 'BX17',
+  label: 'BX17 - East Tremont'
+},
+{
+  value: 'SI14',
+  label: 'SI14 - Grasmere-Arrochar-Ft. Wadsworth'
+},
+{
+  value: 'BK42',
+  label: 'BK42 - Flatbush'
+},
+{
+  value: 'SI36',
+  label: 'SI36 - Old Town-Dongan Hills-South Beach'
+},
+{
+  value: 'BX22',
+  label: 'BX22 - North Riverdale-Fieldston-Riverdale'
+},
+{
+  value: 'BX29',
+  label: 'BX29 - Spuyten Duyvil-Kingsbridge'
+},
+{
+  value: 'BX62',
+  label: 'BX62 - Woodlawn-Wakefield'
+},
+{
+  value: 'BX36',
+  label: 'BX36 - University Heights-Morris Heights'
+},
+{
+  value: 'SI25',
+  label: 'SI25 - Oakwood-Oakwood Beach'
+},
+{
+  value: 'BX30',
+  label: 'BX30 - Kingsbridge Heights'
+},
+{
+  value: 'QN60',
+  label: 'QN60 - Kew Gardens'
+},
+{
+  value: 'BX05',
+  label: 'BX05 - Bedford Park-Fordham North'
+},
+{
+  value: 'BX75',
+  label: 'BX75 - Crotona Park East'
+},
+{
+  value: 'BX37',
+  label: 'BX37 - Van Nest-Morris Park-Westchester Square'
+},
+{
+  value: 'QN17',
+  label: 'QN17 - Forest Hills'
+},
+{
+  value: 'MN34',
+  label: 'MN34 - East Harlem North'
+},
+{
+  value: 'BX49',
+  label: 'BX49 - Pelham Parkway'
+},
+{
+  value: 'MN31',
+  label: 'MN31 - Lenox Hill-Roosevelt Island'
+},
+{
+  value: 'QN68',
+  label: 'QN68 - Queensbridge-Ravenswood-Long Island City'
+},
+{
+  value: 'BX39',
+  label: 'BX39 - Mott Haven-Port Morris'
+},
+{
+  value: 'BX55',
+  label: 'BX55 - Soundview-Bruckner'
+},
+{
+  value: 'QN42',
+  label: 'QN42 - Oakland Gardens'
+},
+{
+  value: 'BX14',
+  label: 'BX14 - East Concourse-Concourse Village'
+},
+{
+  value: 'BK38',
+  label: 'BK38 - DUMBO-Vinegar Hill-Downtown Brooklyn-Boerum Hill'
+},
+{
+  value: 'BX08',
+  label: 'BX08 - West Farms-Bronx River'
+},
+{
+  value: 'BX46',
+  label: 'BX46 - Parkchester'
+},
+{
+  value: 'QN21',
+  label: 'QN21 - Middle Village'
+},
+{
+  value: 'SI08',
+  label: 'SI08 - Grymes Hill-Clifton-Fox Hills'
+},
+{
+  value: 'SI35',
+  label: 'SI35 - New Brighton-Silver Lake'
+},
+{
+  value: 'BK64',
+  label: 'BK64 - Prospect Heights'
+},
+{
+  value: 'QN03',
+  label: 'QN03 - Springfield Gardens South-Brookville'
+},
+{
+  value: 'BK31',
+  label: 'BK31 - Bay Ridge'
+},
+{
+  value: 'BX28',
+  label: 'BX28 - Van Cortlandt Village'
+},
+{
+  value: 'QN29',
+  label: 'QN29 - Elmhurst'
+},
+{
+  value: 'QN50',
+  label: 'QN50 - Elmhurst-Maspeth'
+},
+{
+  value: 'QN28',
+  label: 'QN28 - Jackson Heights'
+},
+{
+  value: 'BK73',
+  label: 'BK73 - North Side-South Side'
+},
+{
+  value: 'BK33',
+  label: 'BK33 - Carroll Gardens-Columbia Street-Red Hook'
+},
+{
+  value: 'QN19',
+  label: 'QN19 - Glendale'
+},
+{
+  value: 'BK37',
+  label: 'BK37 - Park Slope-Gowanus'
+},
+{
+  value: 'MN03',
+  label: 'MN03 - Central Harlem North-Polo Grounds'
+},
+{
+  value: 'BK23',
+  label: 'BK23 - West Brighton'
+},
+{
+  value: 'BK83',
+  label: 'BK83 - Cypress Hills-City Line'
+},
+{
+  value: 'MN14',
+  label: 'MN14 - Lincoln Square'
+},
+{
+  value: 'MN33',
+  label: 'MN33 - East Harlem South'
+},
+{
+  value: 'MN09',
+  label: 'MN09 - Morningside Heights'
+},
+{
+  value: 'MN12',
+  label: 'MN12 - Upper West Side'
+},
+{
+  value: 'BX26',
+  label: 'BX26 - Highbridge'
+},
+{
+  value: 'MN35',
+  label: 'MN35 - Washington Heights North'
+},
+{
+  value: 'MN36',
+  label: 'MN36 - Washington Heights South'
+},
+{
+  value: 'BX63',
+  label: 'BX63 - West Concourse'
+},
+];
+
+export default   labels;
