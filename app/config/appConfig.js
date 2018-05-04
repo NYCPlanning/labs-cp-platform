3693d3ec31d18d47a3e0cb088f9e7405b0ae4f20
Jonathan Pichot <jonathan@pichot.us>
2018-05-04 10:28:23 -0400

    new token

--- app/config/appConfig.js
+++ app/config/appConfig.js
@@ -1,7 +1,7 @@
 const appConfig = {
   auth0_client_id: '3bulG9YPLTsoujIHvFg91w04HNIODCu1',
   auth0_domain: 'cpmanage.auth0.com',
-  mapbox_accessToken: 'pk.eyJ1IjoiY3dob25nbnljIiwiYSI6ImNpczF1MXdrdjA4MXcycXA4ZGtyN2x5YXIifQ.3HGyME8tBs6BnljzUVIt4Q',
+  mapbox_accessToken: 'pk.eyJ1IjoiY2FwaXRhbHBsYW5uaW5nbnljIiwiYSI6ImNqODUwYmxyYzBnY3AycW9hOXA5NDE2eDQifQ.HYuWjTiwSoTu-QLWo0D76w',
   carto_domain: 'planninglabs.carto.com',
   ga_tracking_code: 'UA-84250233-2',
 };
