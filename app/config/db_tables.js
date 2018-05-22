a5f3b7de4c8a354e0a637d96f8e06b17f26e81c5
Jonathan Pichot <jonathan@pichot.us>
2018-05-22 14:49:27 -0400

    Updated cpdb

--- app/config/db_tables.js
+++ app/config/db_tables.js
@@ -1,12 +1,12 @@
 const db_tables = {
   cpdb: {
-    budgets: 'cpdb_budgets_201802_v1',
-    commitments: 'cpdb_commitments_201802_v1',
-    projects: 'cpdb_projects_201802_v1',
-    projects_combined: 'cpdb_projects_combined_201802_v1',
-    adminbounds: 'cpdb_adminbounds_201802_v1',
-    points: 'cpdb_dcpattributes_pts_201802_v1',
-    polygons: 'cpdb_dcpattributes_poly_201802_v1',
+    budgets: 'cpdb_budgets_201805_v1',
+    commitments: 'cpdb_commitments_201805_v1',
+    projects: 'cpdb_projects_201805_v1',
+    projects_combined: 'cpdb_projects_combined_201805_v1',
+    adminbounds: 'cpdb_adminbounds_201805_v1',
+    points: 'cpdb_dcpattributes_pts_201805_v1',
+    polygons: 'cpdb_dcpattributes_poly_201805_v1',
   },
   cb_budget_requests: {
     points: 'cbbr_fy18_pts_v5',
