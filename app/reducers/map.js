9fee7cb3c8bb8299783cb9bd50207b377ad5996f
Tyler Matteo <tylermatteo434@gmail.com>
2021-12-16 11:55:29 -0500

    Remove community board budget request data

--- app/reducers/map.js
+++ app/reducers/map.js
@@ -7,7 +7,6 @@ export const initialState = {
 const mapReducer = (state = initialState, action) => {
   if (action.type === AT.FETCH_FACILITY_DETAILS.SUCCESS ||
       action.type === AT.FETCH_CAPITAL_PROJECT_DETAILS.SUCCESS ||
-      action.type === AT.FETCH_CB_BUDGET_REQUEST_DETAILS.SUCCESS ||
       action.type === AT.FETCH_HOUSING_DEVELOPMENT_DETAILS.SUCCESS ||
       action.type === AT.FETCH_SCA_DETAILS.SUCCESS) {
     return Object.assign({}, state, { centerOnGeometry: action.payload.features[0].geometry });
