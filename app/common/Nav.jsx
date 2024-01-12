cc741e0043ebee470fc0a5e0faff7bca0cfe5968
Tyler Matteo <tylermatteo434@gmail.com>
2024-01-12 13:12:47 -0500

    Revert "Merge user interview sign up banner to master"

--- app/common/Nav.jsx
+++ app/common/Nav.jsx
@@ -1,28 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router";
-import { connect } from "react-redux";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
-import * as authActions from "../actions/auth";
+import * as authActions from '../actions/auth';
 
-import "./Nav.scss";
+import './Nav.scss';
 
 const Nav = (props) => {
   const profile = props.profile;
+
   return (
-    <nav className={"navbar navbar-default navbar-fixed-top"}>
-      <div className="info-banner">
-        <p>
-          In an effort to better serve the needs of users like you, we'd like to
-          hear from you about how we can improve services like this site.&nbsp;
-          <a
-            href="https://cdn.forms-content.sg-form.com/28f9b3d0-9909-11ee-8461-5ad38fb68638"
-            target="_blank"
-          >
-            Click here to sign up.
-          </a>
-        </p>
-      </div>
+    <nav className={'navbar navbar-default navbar-fixed-top'}>
       <div className="navbar-header">
         <button
           type="button"
@@ -37,23 +26,17 @@ const Nav = (props) => {
           <span className="icon-bar" />
           <span className="icon-bar" />
         </button>
-        <Link className="navbar-brand" to="/">
-          &#8291;
-        </Link>
-        <div className="navbar-title">Capital Planning Explorer</div>
+        <Link className="navbar-brand" to="/">&#8291;</Link>
+        <div className="navbar-title">
+          Capital Planning Explorer
+        </div>
       </div>
       <div id="navbar" className="navbar-collapse collapse">
         <ul className="nav navbar-nav navbar-right ">
-          <li>
-            <Link to="/map"> Map</Link>
-          </li>
-          <li>
-            <Link to="/table"> Table</Link>
-          </li>
-          <li style={{ padding: "13px 0", fontSize: "17px" }}> | </li>
-          <li>
-            <Link to={props.about}>About</Link>
-          </li>
+          <li><Link to="/map"> Map</Link></li>
+          <li><Link to="/table"> Table</Link></li>
+          <li style={{ padding: '13px 0', fontSize: '17px' }}> | </li>
+          <li><Link to={props.about}>About</Link></li>
         </ul>
       </div>
     </nav>
@@ -61,8 +44,8 @@ const Nav = (props) => {
 };
 
 Nav.defaultProps = {
-  title: "",
-  about: "/about",
+  title: '',
+  about: '/about',
   children: null,
   profile: {},
 };
